bd1a8a3fbc81032271af3910454770df7f3ceecf
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-08 19:33:07 +0500

    main.1234

---
+++ Typecasting.py
@@ -0,0 +1,19 @@
+name= "Tom"
+age=25
+weight=76.4
+is_student=False
+print("name: ",name)
+print("Data type of name is: ",type(name))
+print("age: ",age)
+print("Data type of age is: ",type(age))
+print("weight: ",weight)
+print("Data type of weight is: ",type(weight))
+print("is student: ",is_student)
+print("Data type of is_student is: ",type(is_student))
+print("after typecasting ")
+age=str(age)
+print("age: ",age)
+print("Data type of age is: ",type(age))
+weight=int(weight)
+print("weight: ",weight)
+print("Data type of weight is: ",type(weight))
